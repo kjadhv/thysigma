5cc4e11c68aa7fc85f9dcffd57f72d54354931c0
kjadhv <kalyani04022003@gmail.com>
2025-11-25 12:53:31 +0530

    Updated code and removed unused images

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -14,7 +14,7 @@ import FooterTwo from "./footer/FooterTwo";
 // import FooterThree from "./footer/FooterThree";
 // import FooterFour from "./footer/FooterFour";
 // import FooterFive from "./footer/FooterFive";
-import VideoModal from "./VideoModal";
+// import VideoModal from "./VideoModal";
 import ScrollProgressBtn from "./ScrollProgressBtn";
 import CustomCursor from "./CustomCursor";
 import SplitType from "split-type";
@@ -229,7 +229,7 @@ const Layout = ({
         {/* {footer === 3 && <FooterThree />}
         {footer === 4 && <FooterFour />}
         {footer === 5 && <FooterFive />} */}
-        {video ? <VideoModal /> : null}
+        {/* {video ? <VideoModal /> : null} */}
         <ScrollProgressBtn />
         <CustomCursor
           onTitleMouseEnter={handleMouseEnterTitle}
